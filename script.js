30dfae6f5b88367a5d0538b8984c8da8d0a63b50
viatovegas <145833157+viatovegas@users.noreply.github.com>
2023-10-16 14:54:53 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 // JavaScript
-console.log('Hello world!');
+const main = document.querySelector('#main-image');
+const thumbs = document.querySelectorAll('.thumb');
+const text = document.querySelector('#caption')
+
+// 確認
+// console.log(main);
+// console.log(thumbs);
+
+thumbs.forEach(function (item, index) {
+    item.onmouseover = function () {
+        main.src = this.dataset.image;
+        text.textContent = this.title;
+    }
+});
